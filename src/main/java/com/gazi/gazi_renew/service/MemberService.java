672746b21616a2c45aa6ef3e31f516a028acebce
WindowH22 <dlckdgml1235@naver.com>
2023-09-25 16:19:54 +0900

    feat: Add MemberService #1 - 로그인 기능구현 - 로그아웃 기능구현 - 토큰 재발급 기능구현

---
+++ src/main/java/com/gazi/gazi_renew/service/MemberService.java
@@ -0,0 +1,18 @@
+package com.gazi.gazi_renew.service;
+
+
+import com.gazi.gazi_renew.dto.MemberRequest;
+import com.gazi.gazi_renew.dto.Response.Body;
+import org.springframework.http.ResponseEntity;
+
+
+public interface MemberService {
+    // 로그인
+    ResponseEntity<Body> login(MemberRequest.Login loginDto);
+
+    // 로그아웃
+    ResponseEntity<Body> logout(MemberRequest.Logout logoutDto);
+
+    // 자동 로그인
+    ResponseEntity<Body> reissue(MemberRequest.Reissue reissueDto);
+}
