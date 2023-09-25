0b405858a5e60e6cf483d17acb454a6f545c2fb6
WindowH22 <dlckdgml1235@naver.com>
2023-09-25 11:25:50 +0900

    conf: 초기 프로젝트 파일 생성

---
+++ src/main/java/com/gazi/gazi_renew/GaziRenewApplication.java
@@ -0,0 +1,13 @@
+package com.gazi.gazi_renew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaziRenewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaziRenewApplication.class, args);
+    }
+
+}
