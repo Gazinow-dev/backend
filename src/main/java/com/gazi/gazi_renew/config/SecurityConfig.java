84336ad460339a45acafecede35626142ac9aa8a
WindowH22 <dlckdgml1235@naver.com>
2023-09-25 16:11:45 +0900

    conf: Spring security setting #1

---
+++ src/main/java/com/gazi/gazi_renew/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.gazi.gazi_renew.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final RedisTemplate redisTemplate;
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return
+                http
+                        .csrf(CsrfConfigurer::disable)
+                        .cors(Customizer.withDefaults())
+                        .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .logout(Customizer.withDefaults())
+                        .logout(logout -> logout.disable()) // 로그아웃 사용 X
+                        .formLogin(formLogin -> formLogin.disable()) // 폼 로그인 사용 X
+                        .authorizeRequests()
+                        .requestMatchers("/api/v1/member/signup", "/api/v1/member/login", "/api/v1/member/email-confirm", "/api/v1/member/reissue", "/api/v1/member/check-nickname").permitAll()
+
+                        .and()
+                        .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, redisTemplate), UsernamePasswordAuthenticationFilter.class)
+                        .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
