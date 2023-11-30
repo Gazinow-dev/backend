fbed8c6322483e5868d4b9f6a121249f25c0ba80
WindowH22 <dlckdgml1235@naver.com>
2023-11-30 22:55:51 +0900

    feat: My 경로 entity 구현  #11

---
+++ src/main/java/com/gazi/gazi_renew/domain/MyFindLoad.java
@@ -0,0 +1,22 @@
+package com.gazi.gazi_renew.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "MY_FIND_LOAD")
+@Entity
+public class MyFindLoad extends AuditingFields{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+    private String name;
+
+}
---
+++ src/main/java/com/gazi/gazi_renew/domain/MyTransfer.java
@@ -0,0 +1,23 @@
+package com.gazi.gazi_renew.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "MY_TRANSFER")
+@Entity
+public class MyTransfer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String transferName;
+    private String transferLine;
+    @ManyToOne
+    @JoinColumn(name = "my_find_load_id", nullable = false)
+    private MyFindLoad myFindLoad;
+}
