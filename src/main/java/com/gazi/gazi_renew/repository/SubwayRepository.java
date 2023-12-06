1140f7c8f71a43814d8c44a9185868718ebed344
WindowH22 <dlckdgml1235@naver.com>
2023-12-06 22:57:37 +0900

    feat: 지하철 위도 경도 받아오는 함수 개발  #12

--- src/main/java/com/gazi/gazi_renew/repository/SubwayRepository.java
+++ src/main/java/com/gazi/gazi_renew/repository/SubwayRepository.java
@@ -1,7 +1,17 @@
 package com.gazi.gazi_renew.repository;
 
 import com.gazi.gazi_renew.domain.Subway;
+import com.gazi.gazi_renew.dto.SubwayDataResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface SubwayRepository extends JpaRepository<Subway, Long> {
+    @Query("select s.name,s.line from Subway s where s.name Like:name%")
+    List<Double> findByName(@Param("name")String name);
+
+    @Query("select new com.gazi.gazi_renew.dto.SubwayDataResponse(s.lat, s.lng) from Subway s where s.name = :name and s.line = :line")
+    SubwayDataResponse findCoordinateByNameAndLine(@Param("name")String name, @Param("line")String line);
 }
--- src/main/java/com/gazi/gazi_renew/service/SubwayDataService.java
+++ src/main/java/com/gazi/gazi_renew/service/SubwayDataService.java
@@ -2,13 +2,13 @@ package com.gazi.gazi_renew.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gazi.gazi_renew.domain.Subway;
+import com.gazi.gazi_renew.dto.SubwayDataResponse;
 import com.gazi.gazi_renew.repository.SubwayRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.io.IOException;
 
 @Service
@@ -20,13 +20,16 @@ public class SubwayDataService {
     public void saveStationsFromJsonFile() throws IOException {
         ClassPathResource resource = new ClassPathResource("station_coordinate.json");
         ObjectMapper objectMapper = new ObjectMapper();
-        System.out.println(resource.getFile());
-        System.out.println(resource.getURI());
         Subway[] subways = objectMapper.readValue(resource.getFile(), Subway[].class);
 
         for (Subway subway : subways) {
             subwayRepository.save(subway);
         }
         log.info("지하철 정보 업로드 완료");
+    }
+
+    public SubwayDataResponse getCoordinateByNameAndLine(String subwayName, String line) {
+        SubwayDataResponse subwayCoordinates = subwayRepository.findCoordinateByNameAndLine(subwayName, line);
+        return subwayCoordinates;
     }
 }
