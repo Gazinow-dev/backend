2f184cf7ca95ef0b0086f2d4465e3f0d4815cca1
WindowH22 <dlckdgml1235@naver.com>
2024-01-04 23:54:53 +0900

    내 경로 저장 구현

--- src/main/java/com/gazi/gazi_renew/service/MyFindRoadService.java
+++ src/main/java/com/gazi/gazi_renew/service/MyFindRoadService.java
@@ -1,11 +1,13 @@
 package com.gazi.gazi_renew.service;
 
+import com.gazi.gazi_renew.dto.MyFindRoadRequest;
 import com.gazi.gazi_renew.dto.Response;
 import org.springframework.http.ResponseEntity;
 
-public interface MyFindLoadService {
+public interface MyFindRoadService {
     // 경로 추가
-    ResponseEntity<Response.Body> addRoute();
+    ResponseEntity<Response.Body> addRoute(MyFindRoadRequest request);
+
     // 경로 삭제
     ResponseEntity<Response.Body> deleteRoute();
 }
