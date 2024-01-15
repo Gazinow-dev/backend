db8e3bb2ebdc893753dccab7ccc87ec6d2c616c8
WindowH22 <dlckdgml1235@naver.com>
2024-01-15 20:29:00 +0900

    feat: MyFindRoad 삭제로 인한 수정2

--- src/main/java/com/gazi/gazi_renew/repository/MyFindRoadRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gazi.gazi_renew.repository;
-
-import com.gazi.gazi_renew.domain.MyFindRoad;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MyFindRoadRepository extends JpaRepository<MyFindRoad, Long> {
-}
