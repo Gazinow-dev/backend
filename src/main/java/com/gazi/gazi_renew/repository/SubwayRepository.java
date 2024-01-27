ce86eee2559d2ea7021e818c8e5b5ebddd20c665
WindowH22 <dlckdgml1235@naver.com>
2024-01-27 20:32:38 +0900

    repository수정

--- src/main/java/com/gazi/gazi_renew/repository/SubwayRepository.java
+++ src/main/java/com/gazi/gazi_renew/repository/SubwayRepository.java
@@ -1,6 +1,6 @@
 package com.gazi.gazi_renew.repository;
 
-import com.gazi.gazi_renew.domain.Subway;
+import com.gazi.gazi_renew.domain.Station;
 import com.gazi.gazi_renew.dto.SubwayDataResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,12 +8,12 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface SubwayRepository extends JpaRepository<Subway, Long> {
-    @Query("select s.name,s.line from Subway s where s.name Like:name%")
+public interface SubwayRepository extends JpaRepository<Station, Long> {
+    @Query("select s.name,s.line from Station s where s.name Like:name%")
     List<Double> findByName(@Param("name")String name);
 
-    List<Subway> findByNameStartingWith(String name);
+    List<Station> findByNameStartingWith(String name);
 
-    @Query("select new com.gazi.gazi_renew.dto.SubwayDataResponse(s.lat, s.lng) from Subway s where s.name = :name and s.line = :line")
+    @Query("select new com.gazi.gazi_renew.dto.SubwayDataResponse(s.lat, s.lng) from Station s where s.name = :name and s.line = :line")
     SubwayDataResponse findCoordinateByNameAndLine(@Param("name")String name, @Param("line")String line);
 }
