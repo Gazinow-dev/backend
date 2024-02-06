1752e05a40fcf5614360aaf1b46c422cf9889d54
WindowH22 <dlckdgml1235@naver.com>
2024-02-06 10:14:42 +0900

    이슈조회관련 APi 명세

--- src/main/java/com/gazi/gazi_renew/controller/IssueRestController.java
+++ src/main/java/com/gazi/gazi_renew/controller/IssueRestController.java
@@ -1,12 +1,14 @@
 package com.gazi.gazi_renew.controller;
 
+import com.gazi.gazi_renew.dto.Response;
 import com.gazi.gazi_renew.service.IssueService;
 import com.gazi.gazi_renew.service.JsoupService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +21,21 @@ public class IssueRestController {
     @PostMapping
     public void sendEmail() throws Exception {
         jsoupService.getData();
+    }
+
+    @GetMapping
+    public ResponseEntity<Response.Body> getIssue(@RequestParam(name="id") Long id){
+        return issueService.getIssue(id);
+    }
+
+    @GetMapping
+    public ResponseEntity<Response.Body> getIssues(@PageableDefault(page = 0, size = 15, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
+        return issueService.getIssues(pageable);
+    }
+
+    @GetMapping
+    public ResponseEntity<Response.Body> getLineByIssues(@RequestParam(name="line") String line,
+                                                         @PageableDefault(page = 0, size = 15, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
+        return issueService.getLineByIssues(line,pageable);
     }
 }
