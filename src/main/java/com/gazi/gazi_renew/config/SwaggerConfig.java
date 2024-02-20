73893b98dac14f733bab796c921b74d8c79ef654
WindowH22 <dlckdgml1235@naver.com>
2024-02-20 10:08:30 +0900

    CORS 방지 스웨거 설정 수정

--- src/main/java/com/gazi/gazi_renew/config/SwaggerConfig.java
+++ src/main/java/com/gazi/gazi_renew/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,7 +19,7 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI()
+        return new OpenAPI().addServersItem(new Server().url("/"))
                 .components(new Components())
                 .info(apiInfo());
     }
