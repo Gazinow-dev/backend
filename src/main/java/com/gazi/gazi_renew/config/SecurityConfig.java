1a6f86422021c59b80859a0a807093c880bf4fce
LminWoo99 <mw310@naver.com>
2024-09-01 18:13:35 +0900

    fix: JWT 인증이 필요한 API 엔드포인트 보안 설정 수정

--- src/main/java/com/gazi/gazi_renew/config/SecurityConfig.java
+++ src/main/java/com/gazi/gazi_renew/config/SecurityConfig.java
@@ -55,8 +55,8 @@ public class SecurityConfig {
                 .formLogin(formLogin -> formLogin.disable()) // 폼 로그인 사용 X
                 .authorizeRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/api/v1/like/*", "/api/v1/member/logout", "/api/v1/member/reissue", "/api/v1/member/change_nickname", "/api/v1/member/change_password"
-                                        ,"/api/v1/member/delete_member", "/api/v1/my_find_road/*", "/api/v1/recentSearch").authenticated() // 요청에 대해 인증 필요
+                                .requestMatchers("/api/v1/like", "/api/v1/member/logout", "/api/v1/member/reissue", "/api/v1/member/change_nickname", "/api/v1/member/change_password"
+                                        , "/api/v1/member/delete_member", "/api/v1/my_find_road/*", "/api/v1/recentSearch", "/api/v1/recentSearch/*").authenticated() // 요청에 대해 인증 필요
                                 .anyRequest().permitAll()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, redisTemplate), UsernamePasswordAuthenticationFilter.class)
@@ -83,3 +83,4 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 }
+
