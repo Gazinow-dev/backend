aaa3d03799db1c1bb44ae0042ee8b200b3183467
LminWoo99 <mw310@naver.com>
2024-10-25 16:09:14 +0900

    fix : duration 버전 변경

--- src/main/java/com/gazi/gazi_renew/config/JwtTokenProvider.java
+++ src/main/java/com/gazi/gazi_renew/config/JwtTokenProvider.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-import org.threeten.bp.Duration;
 
 import java.security.Key;
 import java.time.Duration;
