6542dc3426e397c00805739d009738211d274c18
LminWoo99 <mw310@naver.com>
2024-11-07 16:14:03 +0900

    fix : FCM ios 이슈 해결 (메세지 구조 변경)

--- src/main/java/com/gazi/gazi_renew/dto/FcmMessageDto.java
+++ src/main/java/com/gazi/gazi_renew/dto/FcmMessageDto.java
@@ -1,5 +1,6 @@
 package com.gazi.gazi_renew.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,6 +19,7 @@ public class FcmMessageDto {
         private FcmMessageDto.Notification notification;
         private String token;
         private FcmMessageDto.Data data;
+        private FcmMessageDto.Apns apns;
     }
 
     @Builder
@@ -34,16 +36,45 @@ public class FcmMessageDto {
     public static class Data {
         private String path;
     }
+    @Builder
+    @AllArgsConstructor
+    @Getter
+    public static class Apns {
+        private Payload payload;
+    }
+    @Builder
+    @AllArgsConstructor
+    @Getter
+    public static class Payload {
+        private Aps aps;
+    }
+    @Builder
+    @AllArgsConstructor
+    @Getter
+    public static class Aps {
+        @JsonProperty("content-available")
+        private int contentAvailable;
+    }
     public static FcmMessageDto createMessage(String firebaseToken, String title, String body, String pathJson) {
         return FcmMessageDto.builder()
-                .message(FcmMessageDto.Message.builder()
+                .message(Message.builder()
                         .token(firebaseToken)
-                        .notification(FcmMessageDto.Notification.builder()
+                        .notification(Notification.builder()
                                 .title(title)
                                 .body(body)
                                 .build()
                         )
-                        .data(FcmMessageDto.Data.builder().path(pathJson)
-                                .build()).build()).validateOnly(false).build();
+                        .data(Data.builder().path(pathJson).build())
+                        .apns(Apns.builder()
+                                .payload(Payload.builder()
+                                        .aps(Aps.builder()
+                                                .contentAvailable(1)  // 설정된 contentAvailable
+                                                .build())
+                                        .build())
+                                .build())
+                        .build())
+                .validateOnly(false)
+                .build();
     }
+
 }
