bb022f0badd1918707b822d4ca5bfc2fc7f8137e
LminWoo99 <mw310@naver.com>
2025-01-05 22:31:59 +0900

    docs : swagger 설정 수정

--- src/main/java/com/gazi/gazi_renew/issue/controller/response/IssueCommentResponse.java
+++ src/main/java/com/gazi/gazi_renew/issue/controller/response/IssueCommentResponse.java
@@ -2,6 +2,7 @@ package com.gazi.gazi_renew.issue.controller.response;
 
 import com.gazi.gazi_renew.common.service.port.ClockHolder;
 import com.gazi.gazi_renew.issue.domain.IssueComment;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
@@ -10,9 +11,13 @@ import java.util.stream.Collectors;
 
 @Getter
 public class IssueCommentResponse {
+    @Schema(description = "댓글 ID", example = "1")
     private final Long issueCommentId;
+    @Schema(description = "댓글 내용", example = "댓글 테스트")
     private final String issueCommentContent;
+    @Schema(description = "작성자", example = "가는길 지금")
     private final String createdBy;
+    @Schema(description = "등록시간", example = "2분 전, 지금, 1일 전")
     private final String agoTime;
     @Builder
     public IssueCommentResponse(Long issueCommentId, String issueCommentContent, String createdBy, String agoTime) {
