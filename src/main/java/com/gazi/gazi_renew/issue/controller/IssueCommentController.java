db91918025567b9332ceba05517a22b8644b6551
LminWoo99 <mw310@naver.com>
2025-01-05 22:13:42 +0900

    fix : 페이지네이션 이슈 해결 및 인증 설정 변경

--- src/main/java/com/gazi/gazi_renew/issue/controller/IssueCommentController.java
+++ src/main/java/com/gazi/gazi_renew/issue/controller/IssueCommentController.java
@@ -11,6 +11,7 @@ import com.gazi.gazi_renew.issue.domain.MyCommentSummary;
 import com.gazi.gazi_renew.issue.domain.dto.IssueCommentCreate;
 import com.gazi.gazi_renew.issue.domain.dto.IssueCommentUpdate;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.headers.Header;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -39,10 +40,11 @@ public class IssueCommentController extends BaseController {
     private final Response response;
     @Operation(summary = "이슈 댓글 작성 API")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "댓글 작성 완료"),
+            @ApiResponse(responseCode = "201", description = "댓글 작성 완료",
+                    content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = IssueCommentResponse.class)) ),
             @ApiResponse(responseCode = "400", description = "댓글 내용은 500자를 넘을 수 없습니다.")
-    }
-    )
+    })
     @PostMapping
     public ResponseEntity<Response.Body> saveComment(@Valid @RequestBody IssueCommentCreate issueCommentCreate) {
         IssueComment issueComment = issueCommentService.saveComment(issueCommentCreate);
@@ -52,26 +54,32 @@ public class IssueCommentController extends BaseController {
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "내가 작성한 댓글 조회 완료",
             headers = @Header(name = AUTHORIZATION, description = "Access Token"),
             content = @Content(mediaType = "application/json",
-                    schema = @Schema(implementation = IssueCommentResponse.class)))})
+                    schema = @Schema(implementation = MyCommentSummaryResponse.class)))})
     @GetMapping
-    public ResponseEntity<Response.Body> getIssueCommentsByMemberId(@PageableDefault(page = 0, size = 15, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+    public ResponseEntity<Response.Body> getIssueCommentsByMemberId(
+            @PageableDefault(page = 0, size = 15, sort = "createdAt", direction = Sort.Direction.DESC)
+            @Parameter(description = "Pageable 파라미터. 기본 정렬은 createdAt DESC입니다. 스웨거로 테스트하실 때, sort 값을 string에서 createdAt으로 바꿔주세요")
+            Pageable pageable) {
         Page<MyCommentSummary> issueComments = issueCommentService.getIssueCommentsByMemberId(pageable);
         return response.success(MyCommentSummaryResponse.fromPage(issueComments), "내가 작성한 댓글 조회 완료", HttpStatus.OK);
     }
     @Operation(summary = "이슈에 달린 댓글 조회 API")
-    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "내가 작성한 댓글 조회 완료",
-            headers = @Header(name = AUTHORIZATION, description = "Access Token"),
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "이슈에 달린 댓글 조회 API",
             content = @Content(mediaType = "application/json",
                     schema = @Schema(implementation = IssueCommentResponse.class)))})
     @GetMapping("/{issueId}")
-    public ResponseEntity<Response.Body> getIssueCommentsByIssueId(@PageableDefault(page = 0, size = 15, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable, @PathVariable Long issueId) {
+    public ResponseEntity<Response.Body> getIssueCommentsByIssueId(@PageableDefault(page = 0, size = 15, sort = "createdAt", direction = Sort.Direction.DESC)
+                                                                       @Parameter(description = "Pageable 파라미터. 기본 정렬은 createdAt DESC입니다. 스웨거로 테스트하실 때, sort 값을 string에서 createdAt으로 바꿔주세요")
+                                                                       Pageable pageable, @PathVariable Long issueId) {
         Page<IssueComment> issueCommentList = issueCommentService.getIssueCommentByIssueId(pageable, issueId);
         return response.success(IssueCommentResponse.fromPage(issueCommentList, clockHolder), "이슈에 달린 댓글 조회 완료", HttpStatus.OK);
     }
 
     @Operation(summary = "댓글 수정 API")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "댓글 수정 완료"),
+            @ApiResponse(responseCode = "201", description = "댓글 수정 완료",
+                    content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = IssueCommentResponse.class))),
             @ApiResponse(responseCode = "400", description = "댓글 내용은 500자를 넘을 수 없습니다.")
     })
     @PatchMapping
